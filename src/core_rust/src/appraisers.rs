4da153254069fe9c0bedaff0269989b90f054b5d
dchrnv <dreeftwood@gmail.com>
2025-11-17 14:54:38 +0100

    v0.28.2 Change license from MIT to GNU Affero GPL v3

--- src/core_rust/src/appraisers.rs
+++ src/core_rust/src/appraisers.rs
@@ -1,3 +1,19 @@
+// NeuroGraph - Высокопроизводительная система пространственных вычислений на основе токенов.
+// Copyright (C) 2024-2025 Chernov Denys
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 //! Reward Appraisers for Intuition Module v2.2
 //!
 //! This module implements the 4 core appraisers that evaluate experience events
